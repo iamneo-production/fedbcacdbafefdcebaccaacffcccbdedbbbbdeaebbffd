2bd2b9af1e4772d159105ac1d583320a966f821c
Iamneo <techadmin@iamneo.ai>
2023-08-04 13:42:02 +0000

    Backup Commit

--- reactapp/src/functions/destructuring.js
+++ reactapp/src/functions/destructuring.js
@@ -1,12 +1,11 @@
 function getUserIpDetails(userResponse = {}) {
-  const { userName, userIp = '0.0.0.0' } = userResponse;
-  return [userName, userIp];
+    const { userName, userIp = '0.0.0.0' } = userResponse;
+    return [userName, userIp];
 }
 
 function getTopThree(studentMarks = []) {
-  const [first=null, second=null, third=null, ...rest] = studentMarks;
-  return [first, second, third].sort();
+    const [first=null, second=null, third=null, ...rest] = studentMarks;
+    return [first, second, third].sort();
 }
 
 export {getUserIpDetails,getTopThree}
-
